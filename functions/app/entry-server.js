c9b236a69b2caedbd7e2a81ce1e6b169c843c5e1
Ryan Florence <rpflorence@gmail.com>
2020-11-07 09:22:06 -0700

    stream it!

--- functions/app/entry-server.js
+++ functions/app/entry-server.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { renderToString } from "react-dom/server";
+import { renderToNodeStream } from "react-dom/server";
 import { Response } from "@remix-run/core";
 import Remix from "@remix-run/react/server";
+import streamString from "node-stream-string";
 
 import App from "./App";
 
@@ -11,13 +12,13 @@ export default function handleRequest(
   responseHeaders,
   remixContext
 ) {
-  let markup = renderToString(
+  let markup = renderToNodeStream(
     <Remix url={request.url} context={remixContext}>
       <App />
     </Remix>
   );
 
-  return new Response("<!DOCTYPE html>" + markup, {
+  return new Response(streamString`<!DOCTYPE html>${markup}`, {
     status: responseStatusCode,
     headers: {
       ...Object.fromEntries(responseHeaders),
