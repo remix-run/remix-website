e33ed058a816827fb2fea93ebf13fc521b5b9874
Ryan Florence <rpflorence@gmail.com>
2021-03-10 16:12:12 -0700

    Remove express dependency

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,10 @@
-const express = require("express");
 const functions = require("firebase-functions");
-const cookieParser = require("cookie-parser");
 const { createRequestHandler } = require("./adapter");
 const admin = require("firebase-admin");
 
 // just do it once at the top!
 admin.initializeApp();
 
-// TODO: do we even need an express app anymore here?
-let app = express();
-app.use(cookieParser());
-
-app.get("/api/playground", require("./api/playground"));
-
-app.all(
-  "*",
-  createRequestHandler({
-    build: require("./build"),
-    getLoadContext(req, res) {
-      return { req, res };
-    },
-  })
+exports.app = functions.https.onRequest(
+  createRequestHandler({ build: require("./build") })
 );
-
-exports.app = functions.https.onRequest(app);
