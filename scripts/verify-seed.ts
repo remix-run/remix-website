1d5a9c036c5ed79c84cbf4e1029d9ac1878bda59
Logan McAnsh <logan@mcan.sh>
2021-11-05 16:50:46 -0400

    chore(ci): skip checking posts rn
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- scripts/verify-seed.ts
+++ scripts/verify-seed.ts
@@ -3,16 +3,13 @@ import { PrismaClient } from "@prisma/client";
 let prisma = new PrismaClient();
 
 async function go() {
-  let [refs, posts] = await Promise.all([
-    prisma.gitHubRef.count(),
-    prisma.blogPost.count(),
-  ]);
+  let docs = await prisma.doc.count();
 
-  if (refs && posts) {
+  if (docs) {
     console.log("Seed verified!");
     process.exit(0);
   } else {
-    console.log("Uhh we got problems, no refs or posts found...");
+    console.log("Uhh we got problems, no docs found...");
     process.exit(1);
   }
 }
