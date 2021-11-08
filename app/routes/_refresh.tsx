c5b51030fa0cf98d6a1c8c297e5f47147a8dfd47
Logan McAnsh <logan@mcan.sh>
2021-11-08 11:25:45 -0500

    feat: add docs refreshing
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ app/routes/_refresh.tsx
@@ -0,0 +1,55 @@
+import { RouteComponent, ActionFunction, json } from "remix";
+import { getFlyInstances } from "~/utils/get-fly-instances";
+
+const action: ActionFunction = async ({ request }) => {
+  // verify post request
+  if (request.method !== "POST") {
+    throw new Response("", { status: 405 });
+  }
+
+  // verify the token matches
+  if (request.headers.get("Authorization") !== process.env.AUTH_TOKEN) {
+    throw new Response("", { status: 401 });
+  }
+
+  const { search, searchParams } = new URL(request.url);
+
+  console.log(
+    `Refreshing docs for all instances for ref ${searchParams.get("ref")}`
+  );
+
+  try {
+    // get all app instances and refresh them
+    const instances = await getFlyInstances();
+
+    for (const instance of instances) {
+      const url = new URL(instance);
+      url.pathname = "/_refreshlocal";
+      url.search = search;
+
+      console.log(`forwarding post to ${url.toString()}`);
+
+      // we purposefully don't await, we're just notifying everybody
+      fetch(url.toString(), {
+        method: "POST",
+        headers: {
+          Authorization: process.env.AUTH_TOKEN!,
+        },
+      });
+    }
+
+    console.log(instances);
+
+    return json({ instances });
+  } catch (error) {
+    console.error(error);
+    return json({ ok: false }, { status: 500 });
+  }
+};
+
+const RefreshAllInstancesDocsPage: RouteComponent = () => {
+  return <p>nothing to see here</p>;
+};
+
+export default RefreshAllInstancesDocsPage;
+export { action };
---
+++ app/routes/_refreshlocal.tsx
@@ -0,0 +1,63 @@
+import { RouteComponent, ActionFunction, json } from "remix";
+
+import { GitHubRelease } from "~/@types/github";
+import { saveDocs } from "~/utils/docs/save-docs";
+
+if (!process.env.AUTH_TOKEN) {
+  throw new Error("AUTH_TOKEN env var is not set");
+}
+
+let action: ActionFunction = async ({ request }) => {
+  const url = new URL(request.url);
+
+  // verify post request
+  if (request.method !== "POST") {
+    throw new Response("", { status: 405 });
+  }
+
+  // verify the token matches or doing it locally
+  if (
+    request.headers.get("Authorization") !== process.env.AUTH_TOKEN &&
+    url.hostname !== "localhost"
+  ) {
+    throw new Response("", { status: 401 });
+  }
+
+  const ref = url.searchParams.get("ref");
+
+  console.log(`Refreshing docs for ${url.hostname} for ref ${ref}`);
+
+  if (!ref) {
+    throw new Response('missing "ref" query parameter', { status: 400 });
+  }
+
+  try {
+    // generate docs for specified ref
+    let tag = ref.replace(/^refs\/tags\//, "");
+
+    const releasePromise = await fetch(
+      `https://api.github.com/repos/${process.env.REPO}/releases/tags/${tag}`,
+      {
+        headers: {
+          accept: "application/vnd.github.v3+json",
+        },
+      }
+    );
+
+    const release = (await releasePromise.json()) as GitHubRelease;
+
+    await saveDocs(ref, release.body);
+
+    return json({ ok: true }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return json({ ok: true }, { status: 500 });
+  }
+};
+
+const RefreshInstance: RouteComponent = () => {
+  return <p>nothing to see here</p>;
+};
+
+export default RefreshInstance;
+export { action };
