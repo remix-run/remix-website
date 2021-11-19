52b353de5cfa734550a279499dbca52af7ea849c
Ryan Florence <rpflorence@gmail.com>
2021-11-19 14:47:04 -0700

    make hash links to the bottom header look way better

--- app/components/doc.tsx
+++ app/components/doc.tsx
@@ -33,6 +33,7 @@ const DocsPage: React.VFC = () => {
         })}
         dangerouslySetInnerHTML={{ __html: doc.html }}
       />
+      <div className="h-[90vh]" />
     </PrefetchMarkdownLinks>
   );
 };
