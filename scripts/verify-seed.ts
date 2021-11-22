bf837628a55c9a4f00b90120aa5ed18febe96c85
Logan McAnsh <logan@mcan.sh>
2021-11-22 15:50:49 -0500

    chore(verify-seed): remove verifying blog posts
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- scripts/verify-seed.ts
+++ scripts/verify-seed.ts
@@ -3,21 +3,16 @@ import { PrismaClient } from "@prisma/client";
 let prisma = new PrismaClient();
 
 async function go() {
-  let [docs, posts] = await Promise.all([
-    prisma.doc.count(),
-    prisma.blogPost.count(),
-  ]);
+  let docs = await prisma.doc.count();
 
-  if (docs && posts) {
+  if (docs) {
     console.log("Seed verified!");
     process.exit(0);
   } else {
     if (docs === 0) {
       console.log("Uhh we got problems, no docs found...");
-    } else if (posts === 0) {
-      console.log("Uhh we got problems, no posts found...");
+      process.exit(1);
     }
-    process.exit(1);
   }
 }
 
