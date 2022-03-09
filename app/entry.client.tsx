377ab69716b503f40e5bbf0da4394997c7fb3a26
Kent C. Dodds <me+github@kentcdodds.com>
2022-03-09 16:40:35 -0700

    redirect podcast and add fathom

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,13 @@
 import ReactDOM from "react-dom";
+import { load } from "fathom-client";
 import { RemixBrowser } from "remix";
 
 ReactDOM.hydrate(<RemixBrowser />, document);
+
+if (__env.NODE_ENV !== "development") {
+  load("IRVDGCHK", {
+    url: "https://cdn.usefathom.com/script.js",
+    spa: "history",
+    excludedDomains: ["localhost"],
+  });
+}
