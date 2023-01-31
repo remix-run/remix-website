bf82b4439aca2dfc9d857029c97287bb351d4ec1
Chance Strickland <hi@chance.dev>
2023-01-30 21:36:21 -0800

    reorg tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,59 +4,13 @@
  * Color scheme: https://tailwind.ink?p=7.f9faf9f0f1f1dddfe1b7bcbe8894936a726d54575040413c2c2b2a121212fdfcfbfcf0edf9ccdbf09eb7ee6e90f44250d03150aa253a7d1a264d1014faf9f0f8ef9ffecc1bd3be33a69719837a0b686207514a07373307271f06f2f6f1e0f0deb6e7b56bd96833ad4e22942c1e7e1f1b611b1443170f2913ecf4f3c9eff03defe954cfb71db28b14996313844c13663e0f46300b2c25f5f9fbdff0fcbcdcf98ebbf03992ff4d71da3f55c93240a7232b7a141b4efcfbfbf9eef5f4caece79fd7e571bed83bd2c1338b9a2769701c45441325
  *
  */
+const aspectRatioPlugin = require("@tailwindcss/aspect-ratio");
 const defaultTheme = require("tailwindcss/defaultTheme");
-
-/** @type {Record<string, [string, React.CSSProperties]>} */
-const baseFontSizes = {
-  xs: ["0.75rem", { lineHeight: "1.333" }], // 12px
-  sm: ["0.875rem", { lineHeight: "1.425" }], // 14px
-  base: ["1rem", { lineHeight: "1.5" }], // 16px
-  lg: ["1.125rem", { lineHeight: "1.556" }], // 18px
-  xl: ["1.25rem", { lineHeight: "1.556" }], // 20px
-  "2xl": ["1.5rem", { lineHeight: "1.333" }], // 24px
-  "3xl": ["1.875rem", { lineHeight: "1.2" }], // 30px
-  "4xl": ["2.25rem", { lineHeight: "1.111" }], // 36px
-  "5xl": ["3rem", { lineHeight: "1" }], // 48px
-  "6xl": ["4rem", { lineHeight: "0.938" }], // 64px
-  "7xl": ["4.5rem", { lineHeight: "0.889" }], // 72px
-};
 
 module.exports = {
   mode: "jit",
   content: ["./app/**/*.{ts,tsx}", "./data/**/*.md", "./tailwind-extras.html"],
-  plugins: [
-    require("@tailwindcss/aspect-ratio"),
-    function ({ addVariant, e }) {
-      addVariant("selected", ({ modifySelectors, separator }) => {
-        modifySelectors(({ className, selector }) => {
-          let pseudo = "";
-          if (/:(hover|focus|focus-within|focus-visible)$/.test(selector)) {
-            let i = selector.lastIndexOf(":");
-            if (i != -1) {
-              pseudo = selector.substr(i);
-            }
-          }
-          return `.${e(
-            `selected${separator}${className}`
-          )}[data-selected]${pseudo}`;
-        });
-      });
-      addVariant("expanded", ({ modifySelectors, separator }) => {
-        modifySelectors(({ className }) => {
-          return `.${e(
-            `expanded${separator}${className}`
-          )}[aria-expanded="true"]`;
-        });
-      });
-      addVariant("not-expanded", ({ modifySelectors, separator }) => {
-        modifySelectors(({ className }) => {
-          return `.${e(
-            `not-expanded${separator}${className}`
-          )}[aria-expanded="false"]`;
-        });
-      });
-    },
-  ],
+  plugins: [aspectRatioPlugin, selectedVariantPlugin],
   theme: {
     fontFamily: {
       ...defaultTheme.fontFamily,
@@ -69,7 +23,17 @@ module.exports = {
       mono: ["Source Code Pro", ...defaultTheme.fontFamily.mono],
     },
     fontSize: {
-      ...baseFontSizes,
+      xs: ["0.75rem", { lineHeight: "1.333" }], // 12px
+      sm: ["0.875rem", { lineHeight: "1.425" }], // 14px
+      base: ["1rem", { lineHeight: "1.5" }], // 16px
+      lg: ["1.125rem", { lineHeight: "1.556" }], // 18px
+      xl: ["1.25rem", { lineHeight: "1.556" }], // 20px
+      "2xl": ["1.5rem", { lineHeight: "1.333" }], // 24px
+      "3xl": ["1.875rem", { lineHeight: "1.2" }], // 30px
+      "4xl": ["2.25rem", { lineHeight: "1.111" }], // 36px
+      "5xl": ["3rem", { lineHeight: "1" }], // 48px
+      "6xl": ["4rem", { lineHeight: "0.938" }], // 64px
+      "7xl": ["4.5rem", { lineHeight: "0.889" }], // 72px
     },
     container: {
       center: true,
@@ -84,96 +48,125 @@ module.exports = {
       colors: {
         current: "currentColor",
         gray: {
-          50: "#f8fbfc",
-          100: "#eef2f8",
-          200: "#d0d0d0",
-          300: "#b7bcbe",
-          400: "#828282",
-          500: "#6a726d",
-          600: "#3f3f3f",
-          700: "#292929",
-          800: "#1e1e1e",
+          50: "#f7f7f7",
+          100: "#e3e3e3",
+          200: "#c8c8c8",
+          300: "#a4a4a4",
+          400: "#818181",
+          500: "#666666",
+          600: "#515151",
+          700: "#434343",
+          800: "#383838",
           900: "#121212",
         },
         red: {
-          50: "#fdfcfb",
-          100: "#fcf0ed",
-          200: "#f9ccdb",
-          300: "#f09eb7",
-          400: "#ee6e90",
+          50: "#fef2f3",
+          100: "#ffe1e3",
+          200: "#ffc9cd",
+          300: "#fea3aa",
+          400: "#fc6d78",
           500: "#f44250",
-          brand: "#f44250",
-          600: "#d03150",
-          700: "#aa253a",
-          800: "#7d1a26",
-          900: "#4d1014",
+          brand: "#f44250", // hard-coded in embedded SVG for <docs-*> elements
+          600: "#e12130",
+          700: "#bd1825",
+          800: "#9d1722",
+          900: "#821a22",
         },
         yellow: {
-          50: "#faf9f0",
-          100: "#f8ef9f",
-          200: "#fecc1b",
-          brand: "#fecc1b",
-          300: "#d3be33",
-          400: "#a69719",
-          500: "#837a0b",
-          600: "#686207",
-          700: "#514a07",
-          800: "#373307",
-          900: "#271f06",
+          50: "#fffce8",
+          100: "#fff9c2",
+          200: "#fff087",
+          300: "#ffde44",
+          400: "#fecc1b",
+          brand: "#fecc1b", // hard-coded in embedded SVG for <docs-*> elements
+          500: "#eeb004",
+          600: "#cd8701", // hard-coded in embedded SVG for <docs-*> elements
+          700: "#a45f04",
+          800: "#874b0c",
+          900: "#733d10",
         },
         green: {
-          50: "#f2f6f1",
-          100: "#e0f0de",
-          200: "#b6e7b5",
-          300: "#6bd968",
+          50: "#f2fcf1",
+          100: "#dffade",
+          200: "#c1f3bf",
+          300: "#90e88d",
+          400: "#6bd968",
           brand: "#6bd968",
-          400: "#33ad4e",
-          500: "#22942c",
-          600: "#1e7e1f",
-          700: "#1b611b",
-          800: "#144317",
-          900: "#0f2913",
+          500: "#30ba2d",
+          600: "#229920",
+          700: "#1d791c",
+          800: "#1c601b",
+          900: "#184f18",
         },
         aqua: {
-          50: "#ecf4f3",
-          100: "#c9eff0",
-          200: "#3defe9",
+          50: "#effefc",
+          100: "#cafdf8",
+          200: "#94fbf4",
+          300: "#3defe9",
           brand: "#3defe9",
-          300: "#54cfb7",
-          400: "#1db28b",
-          500: "#149963",
-          600: "#13844c",
-          700: "#13663e",
-          800: "#0f4630",
-          900: "#0b2c25",
+          400: "#24dddc",
+          500: "#0cbdc0",
+          600: "#06969b",
+          700: "#0a767b",
+          800: "#0d5e62",
+          900: "#104d51",
         },
         blue: {
-          50: "#DAEEFF",
-          100: "#AAD6FF",
-          200: "#7FBFFF",
-          300: "#59A8FF",
-          400: "#3992ff",
-          brand: "#3992ff",
-          500: "#287BD9",
-          600: "#1A65B3",
-          700: "#0F4F8C",
-          800: "#073966",
-          900: "#022340",
+          50: "#eef7ff",
+          100: "#d9edff",
+          200: "#bce0ff",
+          300: "#8ecdff",
+          400: "#59b0ff",
+          500: "#3992ff",
+          brand: "#3992ff", // hard-coded in embedded SVG for <docs-*> elements
+          600: "#1b6ef5",
+          700: "#1458e1",
+          800: "#1747b6",
+          900: "#193f8f",
         },
         pink: {
-          50: "#fcfbfb",
-          100: "#f9eef5",
-          200: "#f4caec",
-          300: "#e79fd7",
-          400: "#e571be",
-          500: "#d83bd2",
+          50: "#fff4ff",
+          100: "#fde8ff",
+          200: "#fbd1fd",
+          300: "#faadfa",
+          400: "#f77bf6",
+          500: "#ec49e9",
+          600: "#d83bd2",
           brand: "#d83bd2",
-          600: "#c1338b",
-          700: "#9a2769",
-          800: "#701c45",
-          900: "#441325",
+          700: "#ac1fa3",
+          800: "#8d1b85",
+          900: "#731c6b",
         },
       },
     },
   },
 };
+
+function selectedVariantPlugin({ addVariant, e }) {
+  addVariant("selected", ({ modifySelectors, separator }) => {
+    modifySelectors(({ className, selector }) => {
+      let pseudo = "";
+      if (/:(hover|focus|focus-within|focus-visible)$/.test(selector)) {
+        let i = selector.lastIndexOf(":");
+        if (i != -1) {
+          pseudo = selector.substr(i);
+        }
+      }
+      return `.${e(
+        `selected${separator}${className}`
+      )}[data-selected]${pseudo}`;
+    });
+  });
+  addVariant("expanded", ({ modifySelectors, separator }) => {
+    modifySelectors(({ className }) => {
+      return `.${e(`expanded${separator}${className}`)}[aria-expanded="true"]`;
+    });
+  });
+  addVariant("not-expanded", ({ modifySelectors, separator }) => {
+    modifySelectors(({ className }) => {
+      return `.${e(
+        `not-expanded${separator}${className}`
+      )}[aria-expanded="false"]`;
+    });
+  });
+}
