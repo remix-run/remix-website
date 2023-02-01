e0d95678e6c78e1a5c4371056cdd04c5efe89d61
Chance Strickland <hi@chance.dev>
2023-02-01 07:55:02 -0800

    ts improvements

--- app/components/delegate-links.tsx
+++ app/components/delegate-links.tsx
@@ -1,27 +1,24 @@
 import * as React from "react";
-import { PrefetchPageLinks, useNavigate } from "@remix-run/react";
+import { useNavigate } from "@remix-run/react";
 
-function isLinkEvent(event: MouseEvent) {
-  if (!(event.target instanceof HTMLElement)) return;
-  let a = event.target.closest("a");
-  return (
-    a && // is anchor or has anchor parent
-    !a.hasAttribute("data-noprefetch") && // didn't opt out
-    a.hasAttribute("href") && // has an href
-    a.host === window.location.host && // is internal
-    a
-  );
-}
-
-function useDelegatedReactRouterLinks(nodeRef: React.RefObject<HTMLElement>) {
+function useDelegatedReactRouterLinks<E extends HTMLElement>(
+  nodeRef: React.RefObject<E>
+) {
   let navigate = useNavigate();
 
   React.useEffect(() => {
-    let clickHandler = (event: MouseEvent) => {
+    let node = nodeRef.current;
+    let handler = (event: MouseEvent) => {
       if (!nodeRef.current) return;
-      let a = isLinkEvent(event);
+
+      if (!(event.target instanceof HTMLElement)) return;
+
+      let a = event.target.closest("a");
+
       if (
-        a &&
+        a && // is anchor or has anchor parent
+        a.hasAttribute("href") && // has an href
+        a.host === window.location.host && // is internal
         event.button === 0 && // left click
         (!a.target || a.target === "_self") && // Let browser handle "target=_blank" etc.
         !(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey) // not modified
@@ -32,45 +29,13 @@ function useDelegatedReactRouterLinks(nodeRef: React.RefObject<HTMLElement>) {
       }
     };
 
-    if (!nodeRef.current) return;
-
-    nodeRef.current.addEventListener("click", clickHandler);
-
-    return () => {
-      if (!nodeRef.current) return;
-      nodeRef.current.removeEventListener("click", clickHandler);
-    };
-  }, []);
-}
-
-function PrefetchMarkdownLinks({ children }: { children: React.ReactNode }) {
-  let nodeRef = React.useRef<HTMLDivElement>(null);
-  let [page, setPage] = React.useState<null | string>(null);
-
-  React.useEffect(() => {
-    let handler = (event: MouseEvent) => {
-      if (!nodeRef.current) return;
-      let a = isLinkEvent(event);
-      if (a) {
-        let { pathname, search, hash } = a;
-        setPage(pathname + search);
-      }
-    };
-    if (!nodeRef.current) return;
-    nodeRef.current.addEventListener("mouseenter", handler, true);
+    if (!node) return;
+    node.addEventListener("click", handler);
 
     return () => {
-      if (!nodeRef.current) return;
-      nodeRef.current.removeEventListener("mouseenter", handler);
+      node?.removeEventListener("click", handler);
     };
-  }, []);
-
-  return (
-    <div ref={nodeRef}>
-      {children}
-      {page && <PrefetchPageLinks page={page} />}
-    </div>
-  );
+  }, [navigate, nodeRef]);
 }
 
-export { useDelegatedReactRouterLinks, PrefetchMarkdownLinks };
+export { useDelegatedReactRouterLinks };
