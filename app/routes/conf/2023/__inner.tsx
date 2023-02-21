985f50257b0e1950ebac5d76e9bb1f8250870c52
Chance Strickland <hi@chance.dev>
2023-02-20 22:01:27 -0600

    fix conf styles

--- app/routes/conf/2023/__inner.tsx
+++ app/routes/conf/2023/__inner.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "@remix-run/react";
 
 export default function InnerLayout() {
   return (
-    <div className="my-8 md:my-12 xl:my-14">
+    <div className="mt-24 xl:mt-32 py-8 md:py-12 xl:py-14">
       <div className="mx-auto px-6 md:px-8 lg:px-10 sm:max-w-2xl md:max-w-3xl lg:max-w-4xl">
         <Outlet />
       </div>
