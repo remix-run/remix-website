c20eddf566542e8b6987058ec99e520d4a46b78a
Chance Strickland <hi@chance.dev>
2023-03-03 16:57:57 -0800

    Inline `@ryanflorence/md` & `@ryanflorence/mdtut`
    Markdown processing being hidden behind a private package made debugging
    quite difficult and pinned us to several outdated depenendencies. It
    also closed over a memory leak in Shiki that sometimes causes the app
    server to crash and restart.
    
    This commit removes the `@ryanflorence/md` and `@ryanflorence/mdtut`
    packages and replaces them with inline modules adapted to deal with
    these issues. It also installs up-to-date versions of unified and its
    plugins. This should allow us to make changes and add features to
    markdown processing much more easily in the future.

--- app/lib/md.server.ts
+++ app/lib/md.server.ts
@@ -1,11 +1,519 @@
-import parseFrontMatter from "front-matter";
-import { processMarkdown as md } from "@ryanflorence/md";
-
-/**
- * Parses a markdown contents, including frontmatter.
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+/*!
+ * Forked from https://github.com/ryanflorence/md/blob/master/index.ts
+ *
+ * Adapted from
+ * - ggoodman/nostalgie
+ *   - MIT https://github.com/ggoodman/nostalgie/blob/45f3f6356684287a214dab667064ec9776def933/LICENSE
+ *   - https://github.com/ggoodman/nostalgie/blob/45f3f6356684287a214dab667064ec9776def933/src/worker/mdxCompiler.ts
  */
-export async function processMarkdown(contents: string) {
-  let { attributes, body } = parseFrontMatter(contents);
-  let html = await md(body);
-  return { attributes, raw: body, html };
+import path from "path";
+import { getHighlighter, loadTheme } from "shiki";
+import type { Lang } from "shiki";
+import rangeParser from "parse-numeric-range";
+import parseFrontMatter from "front-matter";
+import type * as Mdast from "mdast";
+import type * as Hast from "hast";
+import type * as Unist from "unist";
+import type * as Shiki from "shiki";
+import type * as Unified from "unified";
+
+// This is relative to where this code ends up in the build, not the source
+const DATA_PATH = path.join(__dirname, "..", "data");
+const THEME_PATH = path.join(DATA_PATH, "base16.json");
+
+export interface ProcessorOptions {
+  resolveHref?(href: string): string;
+}
+
+export async function processMarkdown(
+  content: string,
+  options?: ProcessorOptions
+) {
+  let { attributes, body: raw } = parseFrontMatter(content);
+  let processor = await getProcessor(options);
+  let vfile = await processor.process(raw);
+  let html = vfile.value.toString();
+  return { attributes, raw, html };
+}
+
+export async function getProcessor(options?: ProcessorOptions) {
+  let [
+    { unified },
+    { default: remarkGfm },
+    { default: remarkParse },
+    { default: remarkRehype },
+    { default: rehypeSlug },
+    { default: rehypeStringify },
+    { default: rehypeAutolinkHeadings },
+    plugins,
+  ] = await Promise.all([
+    import("unified"),
+    import("remark-gfm"),
+    import("remark-parse"),
+    import("remark-rehype"),
+    import("rehype-slug"),
+    import("rehype-stringify"),
+    import("rehype-autolink-headings"),
+    loadPlugins(),
+  ]);
+
+  return unified()
+    .use(remarkParse)
+    .use(plugins.tocPlugin, options)
+    .use(plugins.stripLinkExtPlugin, options)
+    .use(plugins.remarkCodeBlocksShiki)
+    .use(remarkGfm)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeStringify, { allowDangerousHtml: true })
+    .use(rehypeSlug, { prefix: "md-" })
+    .use(rehypeAutolinkHeadings);
+}
+
+type InternalPlugin<Input, Output> = Unified.Plugin<
+  [ProcessorOptions?],
+  Input,
+  Output
+>;
+
+let theme: Awaited<ReturnType<typeof loadTheme>>;
+let highlighter: Awaited<ReturnType<typeof getHighlighter>>;
+let fgColor: string;
+let bgColor: string;
+
+export async function loadPlugins() {
+  let [{ visit, SKIP }, { htmlEscape }, { toc }] = await Promise.all([
+    import("unist-util-visit"),
+    import("escape-goat"),
+    import("mdast-util-toc"),
+  ]);
+
+  type MdastNode = Mdast.Root | Mdast.Content;
+
+  // Kinda weird hack, should probably copy/modify mdhast-util-doc but this just
+  // replace any heading with `## toc` in it with the toc
+  const tocPlugin: InternalPlugin<UnistNode.Root, UnistNode.Root> = () => {
+    return async function transformer(tree) {
+      if (tree.children.length === 0) {
+        return {
+          type: "root",
+          children: [],
+        };
+      }
+
+      let first = tree.children[0];
+      if (
+        first.type === "heading" &&
+        first.children[0] &&
+        "value" in first.children[0] &&
+        first.children[0].value === "toc"
+      ) {
+        let result = toc(tree as MdastNode, { heading: "toc" });
+        // remove the `## toc` placeholder
+        tree.children.splice(0, 1);
+
+        // nothing to TOC about (get it?!)
+        if (!result.map) {
+          return getTocWrapper(false, tree.children);
+        }
+
+        result.map.data = { hProperties: { class: "toc" } };
+        return getTocWrapper(result.map, tree.children);
+      } else {
+        return {
+          type: "root",
+          children: [
+            {
+              type: "element",
+              data: {
+                hProperties: {
+                  class: "md-prose",
+                },
+              },
+              children: tree.children,
+            },
+          ],
+        };
+      }
+    };
+  };
+
+  const stripLinkExtPlugin: InternalPlugin<UnistNode.Root, UnistNode.Root> = (
+    options = {}
+  ) => {
+    return async function transformer(tree: UnistNode.Root) {
+      visit(tree, "link", (node, index, parent) => {
+        if (
+          options.resolveHref &&
+          typeof node.url === "string" &&
+          isRelativeUrl(node.url)
+        ) {
+          if (parent && index != null) {
+            parent.children[index] = {
+              ...node,
+              url: options.resolveHref(node.url),
+            };
+            return SKIP;
+          }
+        }
+      });
+    };
+  };
+
+  const remarkCodeBlocksShiki: InternalPlugin<
+    UnistNode.Root,
+    UnistNode.Root
+  > = (options) => {
+    return async function transformer(tree: UnistNode.Root) {
+      let langs: Lang[] = [
+        "js",
+        "json",
+        "jsx",
+        "ts",
+        "tsx",
+        "markdown",
+        "shellscript",
+        "html",
+        "css",
+        "diff",
+        "mdx",
+        "prisma",
+      ];
+      let langSet = new Set(langs);
+      let transformTasks: Promise<void>[] = [];
+
+      visit(tree, "code", (node) => {
+        if (!node.lang || !node.value || !langSet.has(node.lang as Lang)) {
+          return;
+        }
+
+        if (node.lang === "js") node.lang = "javascript";
+        if (node.lang === "ts") node.lang = "typescript";
+        let language = node.lang;
+        let code = node.value;
+
+        // TODO: figure out how this is ever an array?
+        let meta = Array.isArray(node.meta) ? node.meta[0] : node.meta;
+
+        let metaParams = new URLSearchParams();
+        if (meta) {
+          let linesHighlightsMetaShorthand = meta.match(/^\[(.+)\]$/);
+          if (linesHighlightsMetaShorthand) {
+            metaParams.set("lines", linesHighlightsMetaShorthand[0]);
+          } else {
+            metaParams = new URLSearchParams(meta.split(/\s+/).join("&"));
+          }
+        }
+
+        let addedLines = parseLineHighlights(metaParams.get("add"));
+        let removedLines = parseLineHighlights(metaParams.get("remove"));
+        let highlightLines = parseLineHighlights(metaParams.get("lines"));
+        let startValNum = metaParams.has("start")
+          ? Number(metaParams.get("start"))
+          : 1;
+        let startingLineNumber = Number.isFinite(startValNum) ? startValNum : 1;
+        let numbers = !metaParams.has("nonumber");
+
+        transformTasks.push(
+          new Promise(async (res) => {
+            let { tokens, fgColor, bgColor } = await highlight({
+              code,
+              language,
+            });
+
+            let children = tokens.map(
+              (lineTokens, zeroBasedLineNumber): Hast.Element => {
+                let children = lineTokens.map(
+                  (token): Hast.Text | Hast.Element => {
+                    let color = convertFakeHexToCustomProp(token.color || "");
+                    let content: Hast.Text = {
+                      type: "text",
+                      // Do not escape the _actual_ content
+                      value: token.content,
+                    };
+
+                    return color && color !== fgColor
+                      ? {
+                          type: "element",
+                          tagName: "span",
+                          properties: {
+                            style: `color: ${htmlEscape(color)}`,
+                          },
+                          children: [content],
+                        }
+                      : content;
+                  }
+                );
+
+                children.push({
+                  type: "text",
+                  value: "\n",
+                });
+
+                let isDiff = addedLines.length > 0 || removedLines.length > 0;
+                let diffLineNumber = startingLineNumber - 1;
+                let lineNumber = zeroBasedLineNumber + startingLineNumber;
+                let highlightLine = highlightLines?.includes(lineNumber);
+                let removeLine = removedLines.includes(lineNumber);
+                let addLine = addedLines.includes(lineNumber);
+                if (!removeLine) {
+                  diffLineNumber++;
+                }
+
+                return {
+                  type: "element",
+                  tagName: "span",
+                  properties: {
+                    className: "codeblock-line",
+                    dataHighlight: highlightLine ? "true" : undefined,
+                    dataLineNumber: numbers ? lineNumber : undefined,
+                    dataAdd: isDiff ? addLine : undefined,
+                    dataRemove: isDiff ? removeLine : undefined,
+                    dataDiffLineNumber: isDiff ? diffLineNumber : undefined,
+                  },
+                  children,
+                };
+              }
+            );
+            let metaProps: { [key: string]: string } = {};
+            metaParams.forEach((val, key) => {
+              if (key === "lines") return;
+              metaProps[`data-${key}`] = val;
+            });
+
+            let nodeValue = {
+              type: "element",
+              tagName: "pre",
+              properties: {
+                ...metaProps,
+                dataLineNumbers: numbers ? "true" : "false",
+                dataLang: htmlEscape(language),
+                style: `color: ${htmlEscape(
+                  fgColor
+                )};background-color: ${htmlEscape(bgColor)}`,
+              },
+              children: [
+                {
+                  type: "element",
+                  tagName: "code",
+                  children,
+                },
+              ],
+            };
+
+            let data = node.data ?? {};
+            (node as any).type = "element";
+            data.hProperties ??= {};
+            data.hChildren = [nodeValue];
+            node.data = data;
+            res();
+          })
+        );
+
+        return SKIP;
+      });
+
+      await Promise.all(transformTasks);
+
+      async function highlight({
+        code,
+        language,
+      }: {
+        code: string;
+        language: Lang;
+      }) {
+        theme = theme || (await loadTheme(THEME_PATH));
+        highlighter =
+          highlighter || (await getHighlighter({ themes: [theme] }));
+        fgColor =
+          fgColor ||
+          convertFakeHexToCustomProp(
+            highlighter.getForegroundColor(theme.name) || ""
+          );
+        bgColor =
+          bgColor ||
+          convertFakeHexToCustomProp(
+            highlighter.getBackgroundColor(theme.name) || ""
+          );
+
+        let tokens = highlighter.codeToThemedTokens(code, language, theme.name);
+        return {
+          fgColor,
+          bgColor,
+          tokens: tokens,
+        };
+      }
+    };
+  };
+
+  return {
+    tocPlugin,
+    stripLinkExtPlugin,
+    remarkCodeBlocksShiki,
+  };
+
+  function getTocWrapper(
+    toc: Mdast.List | false | null,
+    children: any
+  ): UnistNode.Root {
+    return {
+      type: "root",
+      children: [
+        {
+          type: "element",
+          data: {
+            hProperties: {
+              class: "md-toc-container",
+            },
+          },
+          children: [
+            {
+              type: "element",
+              data: {
+                hProperties: {
+                  class: "md-toc-nav",
+                  role: "navigation",
+                },
+              },
+              // @ts-expect-error
+              children: [toc].filter(Boolean),
+            },
+            {
+              type: "element",
+              data: {
+                hProperties: {
+                  class: "md-prose",
+                },
+              },
+              // @ts-expect-error
+              children,
+            },
+          ],
+        },
+      ],
+    };
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+function parseLineHighlights(param: string | null) {
+  if (!param) return [];
+  let range = param.match(/^\[(.+)\]$/);
+  if (!range) return [];
+  return rangeParser(range[1]);
+}
+
+// The theme actually stores #FFFF${base-16-color-id} because vscode-textmate
+// requires colors to be valid hex codes, if they aren't, it changes them to a
+// default, so this is a mega hack to trick it.
+function convertFakeHexToCustomProp(color: string) {
+  return color.replace(/^#FFFF(.+)/, "var(--base$1)");
+}
+
+function isRelativeUrl(test: string) {
+  // Probably fragile but should work well enough.
+  // It would be nice if the consumer could provide a baseURI we could do
+  // something like:
+  // new URL(baseURI).origin === new URL(test, baseURI).origin
+  let regexp = new RegExp("^(?:[a-z]+:)?//", "i");
+  return !regexp.test(test);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+export namespace UnistNode {
+  export type Content = Flow | Phrasing | Html;
+  export interface Root extends Unist.Parent {
+    type: "root";
+    children: Flow[];
+  }
+
+  export type Flow =
+    | Blockquote
+    | Heading
+    | ParagraphNode
+    | Link
+    | Pre
+    | Code
+    | Image
+    | Element
+    | Html;
+
+  export interface Html extends Unist.Node {
+    type: "html";
+    value: string;
+  }
+
+  export interface Element extends Unist.Parent {
+    type: "element";
+    tagName?: string;
+  }
+
+  export interface CodeElement extends Element {
+    tagName: "code";
+    data?: {
+      meta?: string;
+    };
+    properties?: {
+      className?: string[];
+    };
+  }
+
+  export interface PreElement extends Element {
+    tagName: "pre";
+  }
+
+  export interface Image extends Unist.Node {
+    type: "image";
+    title: null;
+    url: string;
+    alt?: string;
+  }
+
+  export interface Blockquote extends Unist.Parent {
+    type: "blockquote";
+    children: Flow[];
+  }
+
+  export interface Heading extends Unist.Parent {
+    type: "heading";
+    depth: number;
+    children: UnistNode.Phrasing[];
+  }
+
+  interface ParagraphNode extends Unist.Parent {
+    type: "paragraph";
+    children: Phrasing[];
+  }
+
+  export interface Pre extends Unist.Parent {
+    type: "pre";
+    children: Phrasing[];
+  }
+
+  export interface Code extends Unist.Parent {
+    type: "code";
+    value?: string;
+    lang?: Shiki.Lang;
+    meta?: string | string[];
+  }
+
+  export type Phrasing = Text | Emphasis;
+
+  export interface Emphasis extends Unist.Parent {
+    type: "emphasis";
+    children: Phrasing[];
+  }
+
+  export interface Link extends Unist.Parent {
+    type: "link";
+    children: Flow[];
+    url?: string;
+  }
+
+  export interface Text extends Unist.Literal {
+    type: "text";
+    value: string;
+  }
 }
