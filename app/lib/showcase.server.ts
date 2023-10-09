32ff156681fbbc8d93130f86b5bf0db9ba6851f4
Brooks Lybrand <brookslybrand@gmail.com>
2023-10-09 12:25:47 -0500

    Add real assets for above-fold companies

--- app/lib/showcase.server.ts
+++ app/lib/showcase.server.ts
@@ -6,12 +6,12 @@ import yaml from "yaml";
 const dataPath = path.join(__dirname, "..", "data");
 
 // Is it okay to just read and export this data. We shouldn't need an LRU cache since
-// we don't have multiple pages, just the list of companies we want to show
-export const showcaseCompanies: ShowcaseCompany[] = yaml.parse(
+// we don't have multiple pages, just the list of examples we want to show
+export const showcaseExamples: ShowcaseExample[] = yaml.parse(
   fs.readFileSync(path.join(dataPath, "showcase.yaml")).toString()
 );
 
-export interface ShowcaseCompany {
+export interface ShowcaseExample {
   name: string;
   description: string;
   link: string;
--- app/routes/showcase.tsx
+++ app/routes/showcase.tsx
@@ -1,17 +1,27 @@
+import type { MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { useRef } from "react";
-import type { ShowcaseCompany } from "~/lib/showcase.server";
-import { showcaseCompanies } from "~/lib/showcase.server";
+import type { ShowcaseExample } from "~/lib/showcase.server";
+import { showcaseExamples } from "~/lib/showcase.server";
 import { Footer } from "~/ui/footer";
 import { Header } from "~/ui/header";
 
+export const meta: MetaFunction = (args) => {
+  return [
+    {
+      title: "Remix Showcase",
+      description: "See who is using Remix to build better websites.",
+    },
+  ];
+};
+
 export async function loader() {
-  return json({ showcaseCompanies });
+  return json({ showcaseExamples });
 }
 
 export default function Showcase() {
-  const { showcaseCompanies } = useLoaderData<typeof loader>();
+  const { showcaseExamples } = useLoaderData<typeof loader>();
   return (
     <div className="flex h-full flex-1 flex-col">
       <Header />
@@ -25,9 +35,9 @@ export default function Showcase() {
             Some quippy comment about how we're really great
           </p>
         </div>
-        <ul className="mt-8 grid w-full max-w-md grid-cols-1 gap-x-8 gap-y-6 self-center md:max-w-3xl md:grid-cols-2 lg:max-w-6xl lg:grid-cols-3 lg:gap-x-12 lg:gap-y-10">
-          {showcaseCompanies.map((company) => (
-            <ShowcaseCard key={company.name} {...company} />
+        <ul className="mt-8 grid w-full max-w-md grid-cols-1 gap-x-8 gap-y-6 self-center md:max-w-3xl md:grid-cols-2 lg:max-w-6xl lg:grid-cols-3 lg:gap-x-8 lg:gap-y-6 xl:gap-x-12 xl:gap-y-10">
+          {showcaseExamples.map((example) => (
+            <ShowcaseCard key={example.name} {...example} />
           ))}
         </ul>
       </main>
@@ -42,7 +52,7 @@ function ShowcaseCard({
   link,
   imgSrc,
   videoSrc,
-}: ShowcaseCompany) {
+}: ShowcaseExample) {
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
   const playVideo = () => {
@@ -53,7 +63,7 @@ function ShowcaseCard({
   };
 
   return (
-    <li className="relative overflow-hidden border border-gray-100 shadow md:rounded-md">
+    <li className="relative overflow-hidden border border-gray-100 shadow dark:border-gray-800 md:rounded-md">
       <div className="aspect-[4/3] object-cover object-top">
         <video
           ref={videoRef}
@@ -66,28 +76,30 @@ function ShowcaseCard({
           poster={imgSrc}
           onFocus={playVideo}
           onBlur={pauseVideo}
+          width={800}
+          height={600}
         >
-          <source src={videoSrc} type="video/mp4" />
+          <source src={videoSrc} type="video/mp4" width={800} height={600} />
           {/* 😬 what is this? */}
           {/* <source
             src="https://cdn.shopify.com/videos/c/vp/6e7b8b8e8d7348dcabc229459f89f529/6e7b8b8e8d7348dcabc229459f89f529.m3u8"
             type="application/x-mpegURL"
           /> */}
         </video>
-        <div className="p-4">
-          <h2 className="font-medium">
-            <a href={link}>
-              {/* Makes the whole card clickable */}
-              <span
-                onMouseOver={playVideo}
-                onMouseOut={pauseVideo}
-                className="absolute inset-0 rounded-3xl"
-              />
-              {name}
-            </a>
-          </h2>
-          <p className="pt-2 text-xs font-light">{description}</p>
-        </div>
+      </div>
+      <div className="p-4">
+        <h2 className="font-medium">
+          <a href={link}>
+            {/* Makes the whole card clickable */}
+            <span
+              onMouseOver={playVideo}
+              onMouseOut={pauseVideo}
+              className="absolute inset-0 rounded-3xl"
+            />
+            {name}
+          </a>
+        </h2>
+        <p className="pt-2 text-xs font-light">{description}</p>
       </div>
     </li>
   );
