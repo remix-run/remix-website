076d6f75f3426073382aa167bea708c89ace0fda
Brooks Lybrand <brookslybrand@gmail.com>
2023-11-14 09:41:51 -0600

    remove extra config and convert _docs routes to flat routes

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,6 @@
 const { createRoutesFromFolders } = require("@remix-run/v1-route-convention");
 
-const v1Directories = ["_docs", "conf", "blog", "healthcheck", "docs", "$"];
+const v1Directories = ["conf", "blog", "healthcheck", "docs", "$"];
 
 /**
  * @type {import('@remix-run/dev').AppConfig}
@@ -9,15 +9,7 @@ const config = {
   ignoredRouteFiles: v1Directories.map((dir) => `**/${dir}*`),
   routes: (defineRoutes) =>
     createRoutesFromFolders(defineRoutes, {
-      ignoredFilePatterns: [
-        ".*",
-        "_ui/**/*",
-        "_ui.(js|jsx|tsx)",
-        "*.ui.(js|jsx|tsx)",
-        "_marketing*",
-        "img*",
-        "showcase*",
-      ],
+      ignoredFilePatterns: ["**/_ui*", "_marketing*"],
     }),
   serverModuleFormat: "cjs",
   watchPaths: "./data",
