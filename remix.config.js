5640f3ba2439e3ea4e4ea51a4e08acb58774d839
Brooks Lybrand <brookslybrand@gmail.com>
2023-11-14 15:53:34 -0600

    Convert conf 2023 files

--- remix.config.js
+++ remix.config.js
@@ -1,22 +1,7 @@
-const { createRoutesFromFolders } = require("@remix-run/v1-route-convention");
-
-const v1Directories = ["conf"];
-
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
 const config = {
-  ignoredRouteFiles: v1Directories.map((dir) => `**/${dir}*`),
-  routes: (defineRoutes) =>
-    createRoutesFromFolders(defineRoutes, {
-      ignoredFilePatterns: [
-        "**/_ui*",
-        "_marketing*",
-        "blog*",
-        "docs*",
-        "img*/**",
-      ],
-    }),
   serverModuleFormat: "cjs",
   watchPaths: "./data",
 };
