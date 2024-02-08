f4b5ce15bb0644c3dfca30e279234b40a5c712c0
Keisuke Takagi <mit@takagi.app>
2024-02-08 11:01:32 +0900

    refactor: update to generate RSS using feed

--- app/routes/_extras.blog.rss[.xml].tsx
+++ app/routes/_extras.blog.rss[.xml].tsx
@@ -1,43 +1,32 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { getBlogPostListings } from "~/lib/blog.server";
-
-const getCData = (text: string) => {
-  return `<![CDATA[${text}]]>`;
-};
+import { Feed } from "feed";
 
 export const loader: LoaderFunction = async () => {
   const blogUrl = `https://remix.run/blog`;
   const posts = await getBlogPostListings();
 
-  const rss = `
-    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-      <channel>
-        <title>Remix Blog</title>
-        <link>${blogUrl}</link>
-        <atom:link href="${blogUrl}/rss.xml" rel="self" type="application/rss+xml" />
-        <description>Thoughts about building excellent user experiences with Remix.</description>
-        <language>en-us</language>
-        <generator>https://github.com/remix-run/remix</generator>
-        <copyright>© Shopify, Inc.</copyright>
-        ${posts
-          .map(post =>
-            `
-            <item>
-              <title>${getCData(post.title)}</title>
-              <description>${getCData(post.summary)}</description>
-              <pubDate>${new Date(post.dateDisplay).toUTCString()}</pubDate>
-              <link>${blogUrl}/${post.slug}</link>
-              <guid>${blogUrl}/${post.slug}</guid>
-            </item>
-          `.trim(),
-          )
-          .join('\n')}
-      </channel>
-    </rss>
-  `.trim();
+  const feed = new Feed({
+    id: blogUrl,
+    title: "Remix Blog",
+    description: "Thoughts about building excellent user experiences with Remix.",
+    link: blogUrl,
+    language: 'en-us',
+    generator: "https://github.com/jpmonette/feed",
+    copyright: "© Shopify, Inc.",
+  });
+  posts.forEach((post) => {
+    const postLink = `${blogUrl}/${post.slug}`;
+    feed.addItem({
+      id: postLink,
+      title: post.title,
+      link: postLink,
+      date: new Date(post.dateDisplay),
+      description: post.dateDisplay,
+    });
+  });
 
-
-  return new Response(rss, {
+  return new Response(feed.rss2(), {
     headers: {
       "Content-Type": "application/xml",
       "Cache-Control": "public, max-age=0",
