a9202f07349e5ea379bbfc98e0f75385951c3696
Brooks Lybrand <brookslybrand@gmail.com>
2024-02-29 09:00:56 -0600

    Fix meta tags

--- app/lib/meta/meta.ts
+++ app/lib/meta/meta.ts
@@ -16,12 +16,12 @@ export const getMeta = ({
 }: CustomMetaArgs) => {
   return [
     { title },
-    { description },
+    { name: "description", content: description },
     { property: "og:url", content: siteUrl },
     { property: "og:title", content: title },
     { property: "og:description", content: description },
     { property: "og:image", content: image },
-    { property: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:card", content: "summary_large_image" },
     { name: "twitter:creator", content: "@remix_run" },
     { name: "twitter:site", content: "@remix_run" },
     { name: "twitter:title", content: title },
