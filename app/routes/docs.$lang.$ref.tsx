736f04b8c2a6617602f67467a74178077eba46a3
Tyson Lawrie <tlawrie@users.noreply.github.com>
2024-05-27 13:11:25 +1000

    feat: remove custom code copy button

--- app/routes/docs.$lang.$ref.tsx
+++ app/routes/docs.$lang.$ref.tsx
@@ -101,7 +101,6 @@ export default function DocsLayout() {
   }, [location]);
 
   let docsContainer = React.useRef<HTMLDivElement>(null);
-  useCodeBlockCopyButton(docsContainer);
 
   return (
     <div className="[--header-height:theme(spacing.16)] [--nav-width:theme(spacing.72)]">
@@ -882,65 +881,4 @@ function useIsActivePath(to: string) {
   let location = navigating ? navigation.location! : currentLocation;
   let match = matchPath(pathname + "/*", location.pathname);
   return Boolean(match);
-}
-
-function useCodeBlockCopyButton(ref: React.RefObject<HTMLDivElement>) {
-  let location = useLocation();
-  React.useEffect(() => {
-    let container = ref.current;
-    if (!container) return;
-
-    let codeBlocks = container.querySelectorAll(
-      "[data-code-block][data-lang]:not([data-nocopy])",
-    );
-    let buttons = new Map<
-      HTMLButtonElement,
-      { listener: (event: MouseEvent) => void; to: number }
-    >();
-
-    for (let codeBlock of codeBlocks) {
-      let button = document.createElement("button");
-      let label = document.createElement("span");
-      button.type = "button";
-      button.dataset.codeBlockCopy = "";
-      button.addEventListener("click", listener);
-
-      label.textContent = "Copy code to clipboard";
-      label.classList.add("sr-only");
-      button.appendChild(label);
-      codeBlock.appendChild(button);
-      buttons.set(button, { listener, to: -1 });
-
-      function listener(event: MouseEvent) {
-        event.preventDefault();
-        let pre = codeBlock.querySelector("pre");
-        let text = pre?.textContent;
-        if (!text) return;
-        navigator.clipboard
-          .writeText(text)
-          .then(() => {
-            button.dataset.copied = "true";
-            let to = window.setTimeout(() => {
-              window.clearTimeout(to);
-              if (button) {
-                button.dataset.copied = undefined;
-              }
-            }, 3000);
-            if (buttons.has(button)) {
-              buttons.get(button)!.to = to;
-            }
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      }
-    }
-    return () => {
-      for (let [button, props] of buttons) {
-        button.removeEventListener("click", props.listener);
-        button.parentElement?.removeChild(button);
-        window.clearTimeout(props.to);
-      }
-    };
-  }, [ref, location.pathname]);
 }
