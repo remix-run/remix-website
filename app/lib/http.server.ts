b7559c4d7aaeda836123f133f7ab0ce3cc626966
Brooks Lybrand <brookslybrand@gmail.com>
2024-08-15 11:07:57 -0500

    Fix double slash redirect

--- app/lib/http.server.ts
+++ app/lib/http.server.ts
@@ -70,7 +70,8 @@ export function safeRedirect(
 export function removeTrailingSlashes(request: Request) {
   let url = new URL(request.url);
   if (url.pathname.endsWith("/") && url.pathname !== "/") {
-    throw redirect(url.pathname.slice(0, -1) + url.search);
+    url.pathname = url.pathname.slice(0, -1);
+    throw redirect(url.toString());
   }
 }
 
