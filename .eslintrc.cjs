fe3afbe4a1d99934492cf339d1e91ff5eb9928c8
Brooks Lybrand <brookslybrand@gmail.com>
2024-11-13 15:57:10 -0600

    remove and replace@remix-run/eslint-config

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+/**
+ * @type {import('@types/eslint').Linter.BaseConfig}
+ */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  env: {
+    node: true,
+  },
+  plugins: ["@typescript-eslint", "react"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "no-unused-vars": "off",
+    "no-inner-declarations": "off",
+    "no-var": "off",
+    "prefer-const": "off",
+    "react/react-in-jsx-scope": "off", // Not needed in modern React
+    "react/prop-types": "off", // We use TypeScript instead
+    "react/no-unescaped-entities": "off",
+    "react/no-children-prop": "off",
+    "react/no-unknown-property": "off",
+    "@typescript-eslint/no-unused-expressions": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/ban-types": "off",
+  },
+};
