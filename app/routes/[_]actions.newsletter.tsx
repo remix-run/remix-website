427339afa2d30f831441d9c2b635cf88526e400e
Aaron Reisman <aaronreisman@sort.xyz>
2024-11-13 15:47:06 -0800

    Remove NewsletterActionData type

--- app/routes/[_]actions.newsletter.tsx
+++ app/routes/[_]actions.newsletter.tsx
@@ -20,14 +20,3 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
   return { error: null, ok: true };
 };
-
-// This is less than ideal, but adding so we can remove SerializeFrom
-export type NewsletterActionData =
-  | {
-      error: string;
-      ok: boolean;
-    }
-  | {
-      error: any;
-      ok: boolean;
-    };
