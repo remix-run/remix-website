ac719d294bb985e413f05abec3f3228f118f01e8
Brooks Lybrand <brookslybrand@gmail.com>
2025-02-07 14:21:47 -0600

    Remix Jam "letter of intent" (#319)
    * first draft of remix jam letter of intent
    
    * fix background colors and header nav
    
    * Apply text styles
    
    * fix newsletter styles
    
    * fix them seats
    
    * add smooth scrolling
    
    * add redirect, add cache control, shuffle files
    
    * Add parallax effect
    
    * Add meta tags
    
    * update assets
    
    * update title design and main content padding and width
    
    * clean up padding and seats sizes
    
    * add Keepsake component and start examples
    
    * add pick, lanyard, and remix sticker
    
    * Add postcard to Keepsakes, update smooth scrolling, and delete old assets
    
    * Remove 3xl screen size
    
    * Implement z-index management for keepsakes and cleanup code
    
    * remove console.log and rename
    
    * Prevent dark mode
    
    * hook up newsletter
    
    * update seat styles to fix padding overlap issue
    
    * update error styles and copy
    
    * Fix typo
    
    * remove comment

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 // All tokens based on Jim Nielson's designs
 // https://www.figma.com/file/6G68ZVNbR6bMHl2p8727xi/www.remix.run?node-id=0%3A1&t=EXGqoelkZXIPTK6B-0
 
-const defaultTheme = require("tailwindcss/defaultTheme");
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {TailwindConfig} */
 export default {
@@ -17,6 +17,7 @@ export default {
     },
     fontFamily: {
       sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      "fira-sans": ["Fira Sans", ...defaultTheme.fontFamily.sans],
       mono: ["Source Code Pro", ...defaultTheme.fontFamily.mono],
     },
     fontSize: {
