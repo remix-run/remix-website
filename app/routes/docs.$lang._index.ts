ae10ad23fbb415aaf75e5c1edba08040aeac15ac
Brooks Lybrand <brookslybrand@gmail.com>
2025-08-21 14:56:43 -0500

    remove docs routes

--- app/routes/docs.$lang._index.ts
+++
@@ -1,6 +0,0 @@
-import { redirect } from "react-router";
-import type { Route } from "./+types/docs.$lang._index";
-
-export async function loader({ params }: Route.LoaderArgs) {
-  return redirect(`/docs/${params.lang}/main`);
-}
--- app/routes/docs._index.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "react-router";
-
-export async function loader() {
-  return redirect("/docs/en/main", { status: 301 });
-}
