bf1ce85c0d65e106288205bd2461f5a4936d02e3
Brooks Lybrand <brookslybrand@gmail.com>
2026-02-12 16:46:26 -0600

    Remove 2022 and 2023 conf routes, data, and utils

--- app/routes/conf/conf.2022._inner.schedule._index.tsx
+++
@@ -1,4 +0,0 @@
-import type { LoaderFunction } from "react-router";
-import { redirect } from "react-router";
-
-export const loader: LoaderFunction = () => redirect("/conf/schedule/may-25");
--- app/routes/conf/conf.2022._inner.speakers._index.tsx
+++
@@ -1,3 +0,0 @@
-import { redirect } from "react-router";
-
-export const loader = () => redirect("/conf/2022#speakers");
