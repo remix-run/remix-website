0346a0cdabc9701e0d5611836b51de59c9824ca8
Matt Brophy <matt@brophy.org>
2026-05-05 16:27:30 -0400

    Bump timeouts

--- remix-test.config.ts
+++ remix-test.config.ts
@@ -4,6 +4,10 @@ export default {
   coverage: process.env.NODE_ENV === "test",
   playwrightConfig: {
     projects: [{ name: "chromium", use: { browserName: "chromium" } }],
+    use: {
+      actionTimeout: 10_000,
+      navigationTimeout: 10_000,
+    },
   },
   setup: "./test/setup.ts",
 } satisfies RemixTestConfig;
